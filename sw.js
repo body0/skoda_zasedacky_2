4690b2d1087a1eac278b65b516fd23c3ca713889
body0 <nykl.jan.3693@gmail.com>
2019-05-14 14:20:18 +0200

    mearge dodatek

--- sw.js
+++ sw.js
@@ -1,14 +1,45 @@
 const cacheName = 'skoda_zasedacky_cache';
 
 const staticAssets = [
-  './',
-  './app.js',
-  './styles.css',
-  './fallback.json',
-  './images/fetch-dog.jpg'
+    './',
+    './src/js/app.js',
+    './src/js/initCamera.js',
+    './src/style/styles.css',
+
+    './node_modules/qr-scanner/qr-scanner.min.js',
+    './node_modules/qr-scanner/qr-scanner-worker.min.js',
+
+    './img/'
 ];
 
 self.addEventListener('install', async function () {
-  const cache = await caches.open(cacheName);
-  cache.addAll(staticAssets);
+    const cache = await caches.open(cacheName);
+    cache.addAll(staticAssets);
 });
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    const url = new URL(request.url);
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(request));
+    } else {
+        event.respondWith(networkFirst(request));
+    }
+});
+
+async function cacheFirst(request) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || fetch(request);
+}
+
+async function networkFirst(request) {
+    const dynamicCache = await caches.open(cacheName + '-dynamic');
+    try {
+        const networkResponse = await fetch(request);
+        dynamicCache.put(request, networkResponse.clone());
+        return networkResponse;
+    } catch (err) {
+        const cachedResponse = await dynamicCache.match(request);
+        return cachedResponse || await caches.match('./monkResponse.json');
+    }
+}  
