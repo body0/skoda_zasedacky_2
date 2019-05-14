259de197f5ce234391c77668e1a1e5c64f5f54c4
body0 <nykl.jan.3693@gmail.com>
2019-05-14 09:07:06 +0200

    basic temple start

--- src/js/initCamera.js
+++ src/js/initCamera.js
@@ -1,25 +1,18 @@
 "use strict"
 
 import QrScanner from '../../node_modules/qr-scanner/qr-scanner.min.js';
+ 
+QrScanner.hasCamera()
+    .then(hasCamera => init())
+    .catch(e => {
+        console.error(e);
+        displayErrorMesage("Camera is not suported in this brouser");
+    })
 
-
-if("mediaDevices" in navigator){
-    var video = document.getElementById('video');
-    navigator.mediaDevices.getUserMedia({video: true})
-        .then(function(stream) {
-            //st = stream;
-            video.srcObject = stream;
-            const qrScanner = new QrScanner(video, result => console.log('decoded qr code:', result));
-        })
-        .catch(function(err) {
-            console.error('INIT FAIL: ' + err);
-            displayErrorMesage("Enable camera in yout brouser settings");
-        }); 
+function init(){
+    const scanner = new QrScanner(video, result => console.log(result));
+    scanner.start();
 }
-else{
-    displayErrorMesage("Camera is not suported in this brouser");
-}
-
 function displayErrorMesage(e){
     var body = document.getElementById('body');
 }
